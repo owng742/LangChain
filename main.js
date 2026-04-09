2549a790d207f61ff76883908bb9446a1bc510f3
owng742 <owng@ucsc.edu>
2026-04-08 20:45:19 -0700

    added files

---
+++ main.js
@@ -0,0 +1,74 @@
+import { createAgent, providerStrategy, tool } from "langchain";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import "dotenv/config";
+import * as z from "zod";
+
+const getWeather = tool(
+  (input) => `It's always sunny in ${input.city}!`,
+  {
+    name: "get_weather",
+    description: "Get the weather for a given city",
+    schema: z.object({
+      city: z.string().describe("The city to get the weather for"),
+    }),
+  }
+);
+
+const multiply = tool(
+  (input) => `${input.x} multiplied by ${input.y} is ${input.x * input.y}`,
+  {
+    name: "multiply",
+    description: "Multiply two numbers",
+    schema: z.object({
+      x: z.number().describe("First number"),
+      y: z.number().describe("Second number"),
+    })
+  }
+)
+
+const divide = tool(
+  (input) => `${input.x} divided by ${input.y} is ${input.x / input.y}`,
+  {
+    name: "divide",
+    description: "Divide two numbers",
+    schema: z.object({
+      x: z.number().describe("First number"),
+      y: z.number().describe("Second number"),
+    })
+  }
+)
+
+const add = tool(
+  (input) => `${input.x} added by ${input.y} is ${input.x + input.y}`,
+  {
+    name: "add",
+    description: "Add two numbers",
+    schema: z.object({
+      x: z.number().describe("First number"),
+      y: z.number().describe("Second number"),
+    })
+  }
+)
+
+const subtract = tool(
+  (input) => `${input.x} subtracted by ${input.y} is ${input.x - input.y}`,
+  {
+    name: "subtract",
+    description: "Subtract two numbers",
+    schema: z.object({
+      x: z.number().describe("First number"),
+      y: z.number().describe("Second number"),
+    })
+  }
+)
+
+const agent = createAgent({
+  model: new ChatGoogleGenerativeAI({model: "gemini-2.5-flash"}),
+  tools: [multiply, divide, add, subtract],
+});
+
+const res = await agent.invoke({
+    messages: [{ role: "user", content: "What do you get when you multiply 5 by 9 and divide that by 10 and add it by 35.5 and subtract it by 3?" }]
+});
+
+console.log(res);
